fc06fb0ab5d6e206c3f5d2ce162d11163503c6d9
MukutDas <mukut@avdevs.com>
2025-07-21 15:54:40 +0530

    modified functions for diffrent neline delimiters

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,18 +1,24 @@
 export const addFn=(numbers)=>{
-    // console.log(numbers);
+    console.log(numbers);
     if(numbers==="") return 0;
+    let delimiter=/,|\n/;
+    if(numbers?.startsWith("//")){
+        const parts= numbers.split("\n")
+        delimiter= new RegExp(parts[0].substring(2))
+        numbers= parts[1]
+    }
     const numbersArr= numbers.
-                      replace(/\n/g,",").
-                      split(",").
+                      split(delimiter).
                       map(Number);
    
-    // console.log(numbersArr)
+    console.log(numbersArr)
     const res= numbersArr.reduce((sum, number)=>sum+number,0);
-    // console.log(res)
+    console.log(res)
     return res
 }
 
 //  addFn("1,2");
 //  addFn("1,2,3,4,5,6");
-//  addFn("1,2\n3")
+//  addFn("1,2\n3");
+//  addFn("//;\n1;2")
 
