af7ca1ce7aef8b04b34c0be17b3b1da73b6b1331
MukutDas <mukut@avdevs.com>
2025-07-21 13:31:16 +0530

    write function to return 0 when pass empty string into it

--- src/test/stringCalculator.test.js
+++ src/test/stringCalculator.test.js
@@ -1,8 +1,10 @@
-import {addFn} from "../src/utils/stringCalculator";
-
+import {addFn} from "../utils/stringCalculator";
+addFn
 describe("String Calculator",()=>{
     // test for empty string
     test("return 0 when the input is empty string",()=>{
         expect(addFn("")).toBe(0)
-    })
+    });
+
+    
 })
---
+++ src/utils/stringCalculator.js
@@ -0,0 +1,3 @@
+export const addFn=(numbers)=>{
+    return 0;
+}
